f9d97594c179b87ea8dac4aee5c3097b3b6f6ef0
riyanachwani <riyanachwani22@gmail.com>
2024-09-04 20:40:19 +0530

    Routes page updated

---
+++ lib/utils/routes.dart
@@ -0,0 +1,11 @@
+class Routes {
+  static String SplashRoute = '/splash';
+  static String LandingRoute = '/landing';
+  static String SignupRoute = '/signup';
+  static String LoginRoute = '/login';
+  static String DashboardRoute = '/dashboard';
+  static String PersonalInfoRoute = '/personalInfo';
+  static String SleepScheduleRoute = '/sleepSchedule';
+  static String ForgotPasswordRoute = '/forgotPassword';
+  static String SettingsRoute = '/settings';
+}
