6352ea3d51a35a64e45ccb444d1904821696fbf3
riyanachwani <riyanachwani22@gmail.com>
2025-01-11 18:35:52 +0530

    Theme added in main

--- lib/main.dart
+++ lib/main.dart
@@ -3,25 +3,41 @@ import 'package:aquatrack/onboarding/landing.dart';
 import 'package:aquatrack/onboarding/splash_screen.dart';
 import 'package:aquatrack/utils/routes.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+
 
 void main() {
   runApp(const MyApp());
+}
+
+class ThemeModel extends ChangeNotifier {
+  ThemeMode _mode = ThemeMode.light;
+  ThemeMode get mode => _mode;
+
+  void toggleTheme() {
+    _mode = _mode == ThemeMode.light ? ThemeMode.dark : ThemeMode.light;
+    notifyListeners();
+  }
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      themeMode: ThemeMode.light,
-      debugShowCheckedModeBanner: false,
-      initialRoute: MyRoutes.splashRoute,
-      routes: {
-        '/': (context) => SplashScreen(),
-        MyRoutes.splashRoute: (context) => const SplashScreen(),
-        MyRoutes.landingRoute: (context) => const LandingPage(),
-        MyRoutes.signupRoute: (context) => const SignupPage(),
-      },
-    );
+    return ChangeNotifierProvider(
+        create: (_) => ThemeModel(),
+        child: Consumer<ThemeModel>(builder: (context, themeModel, child) {
+          return MaterialApp(
+            themeMode: ThemeMode.light,
+            debugShowCheckedModeBanner: false,
+            initialRoute: MyRoutes.splashRoute,
+            routes: {
+              '/': (context) => SplashScreen(),
+              MyRoutes.splashRoute: (context) => const SplashScreen(),
+              MyRoutes.landingRoute: (context) => const LandingPage(),
+              MyRoutes.signupRoute: (context) => const SignupPage(),
+            },
+          );
+        }));
   }
 }
