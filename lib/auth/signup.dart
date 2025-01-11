721677897e29923f5dd02401d3d9ae040f16894a
riyanachwani <riyanachwani22@gmail.com>
2025-01-12 02:04:01 +0530

    Update VSCode settings

--- lib/auth/signup.dart
+++ lib/auth/signup.dart
@@ -11,10 +11,18 @@ class _SignupPageState extends State<SignupPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.white,
-      body: Column(
-        
-      )
-    );
+        backgroundColor: Colors.white,
+        body: Column(
+          children: [
+            Padding(
+              padding: EdgeInsets.only(top: 0),
+              child: Image.asset(
+                "Assets/images/bg4.jpg",
+                width: 428,
+                height: 457,
+              ),
+            ),
+          ],
+        ));
   }
 }
